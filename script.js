dedd2bcf1e2db2d8818340a5cd16d83d82eea0e3
Arjun-clone <arjungopi143@gmail.com>
2025-06-01 18:33:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".multiple-text",{
-    strings:["Software Developer","Business Developer","Entrepreneur"],
+    strings:["Software Developer","Business Developer","Marketer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
